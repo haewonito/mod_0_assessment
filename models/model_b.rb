31a4747055d22ef5693fda6123941c76d9e5c50b
Haewon Jeon <haewonito@gmail.com>
2021-07-22 14:40:20 -0600

    add model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+pet = {
+  first_name: "Sammy",
+  last_name: "Jeon",
+  age: 21,
+  height: 11,
+  is_old: true,
+  is_feisty: true,
+  colors: ["white", "peach", "greay"],
+  fav_act: ["eating", "hide-and-seek", "ping-pong"]
+}

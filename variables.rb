38d967952ae6fb503d707f86245d3998b0e69d63
Haewon Jeon <haewonito@gmail.com>
2021-07-22 14:27:44 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,16 @@
+famous_person = "George Lucas"
+is_active = true
+
+famous_person = "Harrison Ford"
+
+num_3 = 3
+num_9 = 9
+
+sum = num_3 + num_9
+
+animals = ["dogs", "cats", "birds"]
+
+animals.shift
+puts animals
+
+puts famous_person.length

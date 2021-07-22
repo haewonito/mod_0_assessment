f717ef68f45662a434ed8e0b8d21f91f3816442c
Haewon Jeon <haewonito@gmail.com>
2021-07-22 14:34:38 -0600

    add model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  first_name: "Haewon",
+  last_name: "Jeon",
+  age: 42,
+  weight: 110,
+  is_hungry: false,
+  is_hot: true,
+  past_jobs: ["translator", "chef", "teacher"],
+  family: ["Sean", "John", "Sang"]
+}
